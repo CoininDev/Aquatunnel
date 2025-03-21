4ba1202c4b700cd9abcfd893c7cbc1ecdf0ed422
Gabriel DB <119068235+CoininDev@users.noreply.github.com>
2025-03-21 17:30:10 -0300

    Implementing Space constructors

--- Source/Entity.cs
+++ Source/Entity.cs
@@ -58,4 +58,16 @@ public struct Space {
     public Vector2 Position;
     public Vector2 Scale;
     public float Rotation;
+
+    public Space(float x, float y, float w, float h, float r){
+        this.Position = new Vector2(x,y);
+        this.Scale = new Vector2(w,h);
+        this.Rotation = r;
+    }
+
+    public Space(Vector2 pos, Vector2 scale, float r){
+        this.Position = pos;
+        this.Scale = scale;
+        this.Rotation = r;
+    }
 }
