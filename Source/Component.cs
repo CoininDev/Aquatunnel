1f5b79d3d475a145f1e8efd04290187aa040167a
Gabriel DB <119068235+CoininDev@users.noreply.github.com>
2025-03-21 17:24:49 -0300

    Update Component.cs

--- Source/Component.cs
+++ Source/Component.cs
@@ -5,9 +5,6 @@ namespace Aquatunnel;
 
 public abstract class Component {
     protected Entity entity;
-    public Component(Entity entity){
-        this.entity = entity;
-    }
     public abstract void Init();
     public abstract void Update(GameTime time);
     public abstract void Draw(SpriteBatch batch);
