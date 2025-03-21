b40f1ad994848a431ef0114cf6c7eda08576a850
Gabriel DB <119068235+CoininDev@users.noreply.github.com>
2025-03-21 17:39:09 -0300

    Loading texture in Init function

--- Source/Components/Sprite.cs
+++ Source/Components/Sprite.cs
@@ -4,16 +4,20 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Aquatunnel;
 
 public class SpriteComp : Component{
+    public string TextureName;
     public Texture2D Texture;
     
-    public SpriteComp(Entity entity, Texture2D texture) : base(entity){
-        this.Texture = texture;
+    public SpriteComp(string textureName) {
+        this.TextureName = textureName;
     }
 
-    public override void Draw(SpriteBatch batch) 
-    {
+    public override void Init() {
+        Entity.Game.Load<Texture2D>(this.TextureName);
+    }
+    
+    public override void Draw(SpriteBatch batch) {
         batch.Draw(Texture, entity.Space.Scale, Color.White);
     }
-    public override void Init() {}
+    
     public override void Update(GameTime time) {}
 }
